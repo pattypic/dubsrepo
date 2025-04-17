e6e0470f99f4109727c0e67556ea14356799ee8b
Patrick Pichardo <busibbpicha717@gmail.com>
2025-04-17 03:15:32 +0000

    changes

--- something.py
+++ something.py
@@ -1,14 +1,14 @@
-python generate_schema.py \
+!python generate_schema.py \
   --project dubsrepo \
   --dataset dubsrepo_stg \
   --output models/stg/schema.yml
-
-python generate_schema.py \
+print("here")
+!python generate_schema.py \
   --project dubsrepo \
   --dataset dubsrepo_int \
   --output models/int/schema.yml
 
-python generate_schema.py \
+!python generate_schema.py \
   --project dubsrepo \
   --dataset dubsrepo_mrt \
   --output models/mrt/schema.yml
