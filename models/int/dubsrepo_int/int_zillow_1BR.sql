915b8c166e12aa89ba37d0e211aa0e94858f6df9
Patrick Pichardo <busibbpicha717@gmail.com>
2025-04-30 02:27:17 +0000

    comiit

--- models/int/dubsrepo_int/int_zillow_1BR.sql
+++ models/int/dubsrepo_int/int_zillow_1BR.sql
@@ -7,7 +7,10 @@ with dubsrepo_int as (
             "CURRENT_TIMESTAMP() AS load_time ",
             "FROM dubsrepo_stg.zillow_1BR WHERE region = '", region, "';"
         ) AS sql_statement
-        FROM (SELECT DISTINCT region FROM ref('zillow_1BR'));
+        FROM (
+            SELECT DISTINCT region 
+            FROM {{ref('zillow_1BR')}}
+            )
 )
 
 select * from dubsrepo_int
